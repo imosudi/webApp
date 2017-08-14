99b6e36539c3467cdc5bf5ed05ca83f3af5ea9d2
imosudi <imosudi@gmail.com>
2017-08-14 22:11:18 +0100

    Commit Mon Aug 14 22:11:18 WAT 2017

--- setup.py
+++ setup.py
@@ -1,14 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import request
+from flask import redirect, url_for
+from flask.ext.script import Manager
+
 app = Flask(__name__)
 
-@app.route('/')
+manager = Manager(app)
+
+
+
+'''@app.route('/')
 def index():
 	return '<h1>Hello World!</h1>'
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>Hello, %s!</h1>' %name
+	pass
+@app.route('/useragent')
+def browserUserAgent():
+	user_agent = request.headers.get('User-Agent')
+	return '<p> My browser is %s</p>' % user_agent
+	pass
+@app.route('/badrequest')
+def badrequest():
+	return '<h1>Bad request!</h1>', 400
+	pass
+@app.route('/redirect')
+def redirec():
+	return redirect('http://www.mioemi.com')
+	pass'''
 
-
-
-
-
+@app.route('/index')
+def index():
+	return render_template('index.html')
+	pass
 
 
 
@@ -16,4 +41,5 @@ def index():
 
 
 if __name__ == '__main__':
+	manager.run()
 	app.run(debug=True)
