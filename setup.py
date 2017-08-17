4464a6fff9bab75e34dc7000d8edbd455e7cc83f
imosudi <imosudi@gmail.com>
2017-08-17 06:31:14 +0100

    Thu Aug 17 06:31:14 WAT 2017

--- setup.py
+++ setup.py
@@ -46,10 +46,21 @@ def index():
 	return render_template('index.html', current_time=datetime.utcnow())
 	pass"""
 
-@app.route('/')
+"""@app.route('/')
 def index():
 	return render_template('index.html',
 							current_time=datetime.utcnow())
+"""
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', form=form, name=name)
+	pass
 
 @app.route('/user/<name>')
 def user(name):
